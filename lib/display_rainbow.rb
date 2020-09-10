2f82dffc5b9369636ed6bbaae87ff7530583ccb3
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-10 21:29:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-def display_rainbow 
-  display_rainbow = ['red', 'orange', 'yellow'. 'green', 'blue', 'indigo', 'violet']
+def display_rainbow = ['red', 'orange', 'yellow'. 'green', 'blue', 'indigo', 'violet']
   puts "R = #{colors[0]}"
   puts "O = #{colors[1]}" 
   puts "Y = #{colors[2]}"
