246c6ff23ddf75411842cf7f83412354a8631674
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-10 20:59:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,2 @@
 def display_rainbow 
   colors = (['red', 'orange', 'yellow'. 'green', 'blue', 'indigo', 'violet'])
-R = "red"
-O = "orange" 
-Y = "yellow"
-G = "green" 
-B = "blue"
-I = "indigo" 
-V = "violet" 
